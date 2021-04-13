a67f20026259ea9aa261a3ba08daf6fa8471af52
Sato <sato.rc@yandex.ru>
2021-04-13 15:39:47 +0300

    Run prettier:fix

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     },
     devtool: 'source-map',
     resolve: {
-        extensions: ['.ts', '.js']
+        extensions: ['.ts', '.js'],
     },
     module: {
         rules: [
@@ -21,7 +21,7 @@ module.exports = {
                 test: /\.ts$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
-            }
+            },
         ],
     },
 };
