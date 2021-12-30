3052367f513ea466943578692a9614e954d4f8c6
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-12-30 19:22:11 +0200

    Be explicit about what's exported publicly

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
 import './@types/uploadcare-widget';
 
-export * from './constants';
-export * from './lib';
-export * from './types';
+export {
+    UPLOADCARE_CDN_URL,
+    UPLOADCARE_FILE_DATA_KEY,
+    UPLOADCARE_IMAGE_SIZE_LIMIT,
+} from './constants';
+
+export {
+    UploadcareImage,
+    isUploadcareImageSizeValid,
+    UploadcareFile,
+    toProgressPromise,
+    awaitUploads,
+} from './lib';
+
+export type {
+    UploadcareFileStoragePayload,
+    UploadcareImageStoragePayload,
+    UploadcareStoragePayload,
+    PrezlyFileInfo,
+} from './types';
